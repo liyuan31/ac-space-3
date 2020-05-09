5da9d31c9032e2300bb95d401a11ba3f4b053446
Walden Li <walden@waldens-mbp.attlocal.net>
2020-05-09 14:41:24 -0400

    Added animation

--- Visualization/data.js
+++ Visualization/data.js
@@ -2,8 +2,12 @@ class Data {
     constructor(data) {
         // this should be derived from a d3.csv callback
         this.data = data;
-        this.mean_a_opt_b_1 = d3.mean(this.data, d => d.a_opt_b_1);
-        this.mean_a_opt = d3.mean(this.data, d => d.a_opt);
+        const format2 = d3.format(".2f");
+        const format4 = d3.format(".4f");
+        this.mean_a_opt_b_1 = format2(d3.mean(this.data, d => d.a_opt_b_1));
+        this.mean_a_opt = format2(d3.mean(this.data, d => d.a_opt));
+        this.mean_a_sr = format2(d3.mean(this.data, d => d.a_sr));
+        this.mean_a_rt = format4(d3.mean(this.data, d => d.a_rt));
         // const mean_a_opt_b_2 = d3.mean(data, d => d.a_opt_b_2);
         // const mean_a_opt_b_3 = d3.mean(data, d => d.a_opt_b_3);
         // const mean_s_opt_b_1 = d3.mean(data, d => d.s_opt_b_1);
--- Visualization/sidebar.js
+++ Visualization/sidebar.js
@@ -21,21 +21,29 @@ class SidebarWidget {
                 .duration(1000)
                 .attr("x2", `${this.data.mean_a_opt}%`);
         const a_sr = this.content_div.select("#a-sr");
-        a_sr.select(".info").select(".label").html("P. Switch");
-        a_sr.select(".info").select(".value").html(`${this.data.mean_a_opt}%`);
+        a_sr.select(".info").select(".label").html("Switch Rate");
+        a_sr.select(".info").select(".value").html(`${this.data.mean_a_sr}%`);
         a_sr.select(".figure").append("svg").append("line")
             .attr("x1", "0")
             .attr("y1", "0")
-            .attr("x2", `${this.data.mean_a_opt}%`)
-            .attr("y2", "0");
+            .attr("x2", "0")
+            .attr("y2", "0")
+            .transition()
+                .duration(1000)
+                .attr("x2", `${this.data.mean_a_sr}%`);
         const a_rt = this.content_div.select("#a-rt");
         a_rt.select(".info").select(".label").html("Response Time");
-        a_rt.select(".info").select(".value").html(`${this.data.mean_a_opt}%`);
+        a_rt.select(".info").select(".value").html(`${this.data.mean_a_rt}`);
+        // calculate the proportion rt of 6s, since the max rt in the dataset is 5.8
+        const rt_prop = this.data.mean_a_rt/6*100;
         a_rt.select(".figure").append("svg").append("line")
             .attr("x1", "0")
             .attr("y1", "0")
-            .attr("x2", `${this.data.mean_a_opt}%`)
-            .attr("y2", "0");  
+            .attr("x2", "0")
+            .attr("y2", "0")
+            .transition()
+                .duration(1000)
+                .attr("x2", `${rt_prop}%`);  
     }
 
 }
