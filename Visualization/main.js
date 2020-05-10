100f2ac42591d8110d28151ee8fb6eca8fb253c8
Walden Li <walden@waldens-mbp.attlocal.net>
2020-05-10 10:15:34 -0400

    Modified sidebar rendering logic

--- Visualization/main.js
+++ Visualization/main.js
@@ -2,5 +2,5 @@ d3.csv("data/ac_space_3.csv").then(function(data) {
     const data_object = new Data(data);
     const sidebar = new SidebarWidget(data_object);
     const display = new MainWidget();
-    sidebar.render();
+    // sidebar.update();
 })
--- Visualization/sidebar.js
+++ Visualization/sidebar.js
@@ -4,15 +4,28 @@
 class SidebarWidget {
     constructor(data) {
         this.data = data;   // an object from <Data> class
-        this.content_div = d3.select(".side-bar-content");
+        // the parent element of the widget
+        this.parent = d3.select(".side-bar-content");
+        this.update();
     }
 
-    render() {
+    /**
+     * When called, this function updates the bound data object and render the sidebar again.
+     * @param {Data} data : default is the existing <Data> object (no update), replace when given the argument 
+     */
+    update(data=this.data) {
+        this.data = data;
+        this._render();
+    }
+
+    _render() {
         // First render elements for each panel
-        const a_opt = this.content_div.select("#a-opt");
+        const a_opt = this.parent.select("#a-opt");
         a_opt.select(".info").select(".label").html("P. Optimal");
         a_opt.select(".info").select(".value").html(`${this.data.mean_a_opt}%`);
         const a_svg = a_opt.select(".figure").selectAll("svg").data([this.data]);
+        // If <svg> does not exist, i.e., in the intial rendering, the enter selection will have
+        // one sub-selection of <svg>.
         a_svg.enter().append("svg").append("line")
             .attr("x1", "0")
             .attr("y1", "0")
@@ -21,7 +34,20 @@ class SidebarWidget {
             .transition()
                 .duration(1000)
                 .attr("x2", d => `${d.mean_a_opt}%`);
-        const a_sr = this.content_div.select("#a-sr");
+        // When the render function is called again, the enter selection is null, so in order to
+        // update, we need to use the existing selection.
+        a_svg.append("svg").append("line")
+            .attr("x1", "0")
+            .attr("y1", "0")
+            .attr("x2", "0")
+            .attr("y2", "0")
+            .transition()
+                .duration(1000)
+                .attr("x2", d => `${d.mean_a_opt}%`);
+        // Since the data object bound with <svg> will always contain only one element, the exit
+        // selection should always be null. But I'll write the following statement just for completeness.
+        a_svg.exit().remove();
+        const a_sr = this.parent.select("#a-sr");
         a_sr.select(".info").select(".label").html("Switch Rate");
         a_sr.select(".info").select(".value").html(`${this.data.mean_a_sr}%`);
         a_sr.select(".figure").append("svg").append("line")
@@ -32,7 +58,7 @@ class SidebarWidget {
             .transition()
                 .duration(1000)
                 .attr("x2", `${this.data.mean_a_sr}%`);
-        const a_rt = this.content_div.select("#a-rt");
+        const a_rt = this.parent.select("#a-rt");
         a_rt.select(".info").select(".label").html("Response Time");
         a_rt.select(".info").select(".value").html(`${this.data.mean_a_rt}`);
         // calculate the proportion rt of 6s, since the max rt in the dataset is 5.8
