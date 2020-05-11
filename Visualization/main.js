3e3d3deaecf0fd33af59ed637b73b1402d1ef102
Walden Li <walden@waldens-mbp.attlocal.net>
2020-05-11 14:08:05 -0400

    Fixed bug: sub undefined

--- Visualization/main.js
+++ Visualization/main.js
@@ -5,8 +5,15 @@ function main_update(sub) {
 d3.csv("data/ac_space_3.csv").then(data => render(data));
 
 const render = function(data) {
-    console.log(data[sub])
-    sidebar.update(data[sub]);
+    if (sub !== '0') {
+        let i = 0;
+        for (i; i < data.length; i++) {
+            if (data[i].sub === sub) break;
+        }
+        console.log(i);
+        console.log(data[i]);
+        sidebar.update(data[i]);        
+    } else { Error("Error: update called on initialization")}
 }
 }
 
