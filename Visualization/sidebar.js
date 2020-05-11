ea64b45020137aae5ed4a5d3768e1049fb28f6e5
Walden Li <walden@waldens-mbp.attlocal.net>
2020-05-11 15:16:56 -0400

    Fixed a d3 selection problem

--- Visualization/sidebar.js
+++ Visualization/sidebar.js
@@ -10,6 +10,9 @@ class SidebarWidget {
         };   // an object from <Data> class
         // the parent element of the widget
         this.parent = d3.select(".side-bar-content");
+        // the width of the parent element
+        // TODO: this is hard-coded
+        this.width = 252;
         this.update();
     }
 
@@ -23,36 +26,50 @@ class SidebarWidget {
     }
 
     _render() {
+
+        const prop_scale = d3.scaleLinear()
+            .domain([0, 100])
+            .range([0, this.width]);
+        const pink_to_red = d3.scaleLinear()
+            .domain([0, 100])
+            .range(["pink", "red"]);
+
         // First render elements for each panel
         const a_opt = this.parent.select("#a-opt");
         a_opt.select(".info").select(".label").html("P. Optimal");
         a_opt.select(".info").select(".value").html(`${this.data.a_opt}%`);
-        const a_svg = a_opt.select(".figure").selectAll("svg").data([this.data]);
+        a_opt.select(".figure").selectAll("svg").data([0]).enter().append("svg");
+        const a_opt_figure_svg = a_opt.select(".figure").select("svg");
+        const a_opt_figure_svg_line = a_opt_figure_svg.selectAll("line").data([this.data]);
         // If <svg> does not exist, i.e., in the intial rendering, the enter selection will have
         // one sub-selection of <svg>.
-        a_svg.enter().append("svg").append("line")
+        a_opt_figure_svg_line.enter().append("line")
+            .merge(a_opt_figure_svg_line)
             .attr("x1", "0")
             .attr("y1", "0")
             .attr("x2", "0")
             .attr("y2", "0")
             .transition()
                 .duration(1000)
-                .attr("x2", d => `${d.a_opt}%`);
+                .attr("x2", d => `${prop_scale(d.a_opt)}`)
+                .attr("stroke", d => `${pink_to_red(d.a_opt)}`);
         // When the render function is called again, the enter selection is null, so in order to
         // update, we need to use the existing selection.
         // First remove the existing <line>, if any
-        a_svg.selectAll("line").remove();
-        a_svg.append("line")
-            .attr("x1", "0")
-            .attr("y1", "0")
-            .attr("x2", "0")
-            .attr("y2", "0")
-            .transition()
-                .duration(1000)
-                .attr("x2", d => `${d.a_opt}%`);
-        // Since the data object bound with <svg> will always contain only one element, the exit
-        // selection should always be null. But I'll write the following statement just for completeness.
-        a_svg.exit().remove();
+        // a_opt_figure_svg_line.remove();
+        // a_opt_figure_svg_line
+        //     .attr("x1", "0")
+        //     .attr("y1", "0")
+        //     .attr("x2", "0")
+        //     .attr("y2", "0")
+        //     .attr("stroke", pink_to_red(0))
+        //     .transition()
+        //         .duration(1000)
+        //         .attr("x2", d => `${prop_scale(d.a_opt)}`)
+        //         .attr("stroke", d => `${pink_to_red(d.a_opt)}`);
+        // // Since the data object bound with <svg> will always contain only one element, the exit
+        // // selection should always be null. But I'll write the following statement just for completeness.
+        // a_opt_figure_svg_line.exit().remove();
 
         // And do the same thing to switch rate
         const a_sr = this.parent.select("#a-sr");
